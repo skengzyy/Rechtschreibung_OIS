7bd3aea8a8a9e41b8a5d50e692dd86afb5733bd2
skeng <ijerryson@student.tgm.ac.at>
2024-12-19 11:52:20 +0100

    Recommiting progress

--- src/rechtschreibtrainer/view/MenüFrame.java
+++ src/rechtschreibtrainer/view/MenüFrame.java
@@ -1,6 +1,5 @@
 package rechtschreibtrainer.view;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowListener;
--- src/rechtschreibtrainer/view/QuizPanel.java
+++ src/rechtschreibtrainer/view/QuizPanel.java
@@ -4,11 +4,41 @@ import rechtschreibtrainer.controller.TrainerController;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class QuizPanel extends JPanel {
     private TrainerController tc;
+    private JButton nextButton, exitButton;
+    private JLabel label, query;
+
     public QuizPanel(TrainerController tc) {
+        this.tc = tc;
 
+        // Set the layout
+        setLayout(new GridLayout(5, 1, 3, 3));
+        setBackground(Color.LIGHT_GRAY);
 
+        // Create the bottom area
+        JPanel bottomArea = new JPanel();
+        bottomArea.setLayout(new GridLayout(2, 2));
+        bottomArea.setBackground(Color.GRAY);
+
+        // Create the labels and buttons
+        label = new JLabel();
+        label.setFont(new Font("Arial", Font.BOLD, 28));
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        query = new JLabel("");
+        query.setFont(new Font("Arial", Font.PLAIN, 18));
+        query.setHorizontalAlignment(JLabel.CENTER);
+
+        nextButton = new JButton("Next");
+        exitButton = new JButton("Exit");
+
+        // Add the components to the panel
+        this.add(label);
+        this.add(query);
+        bottomArea.add(nextButton);
+        bottomArea.add(exitButton);
+        this.add(bottomArea);
     }
-
 }
