4bce8d62fc49af940f6ed522ae5227f741b9b1e7
skeng <ijerryson@student.tgm.ac.at>
2024-12-22 20:18:52 +0100

    Recommiting progress

--- src/rechtschreibtrainer/model/BildFrage.java
+++ src/rechtschreibtrainer/model/BildFrage.java
@@ -1,4 +1,23 @@
 package rechtschreibtrainer.model;
 
-public class BildFrage {
+public class BildFrage extends Frage{
+    private String loesung;
+
+    public BildFrage(String frage, String loesung) {
+        super(frage);
+        setLoesung(loesung);
+    }
+
+    public String getLoesung() {
+        return loesung;
+    }
+
+    public void setLoesung(String loesung) {
+        if(loesung != null && loesung.isEmpty()) this.loesung = loesung;
+    }
+
+    @Override
+    public boolean check(String antwort) {
+        return super.check(antwort);
+    }
 }
--- src/rechtschreibtrainer/model/BooleanFrage.java
+++ src/rechtschreibtrainer/model/BooleanFrage.java
@@ -1,4 +1,27 @@
 package rechtschreibtrainer.model;
 
-public class BooleanFrage {
+public class BooleanFrage extends Frage{
+    private boolean loesung;
+
+    public BooleanFrage(String frage, boolean loesung) {
+        super(frage);
+        setLoesung(loesung);
+    }
+
+    public boolean isLoesung() {
+        return loesung;
+    }
+
+    public void setLoesung(boolean loesung) {
+         this.loesung = loesung;
+    }
+
+
+    public boolean check(String antwort) {
+        antwort = antwort.toLowerCase();
+        if(antwort.equals("true") || antwort.equals("false")) {
+            return antwort.equals(super.getAntwort());
+        }
+        return false;
+    }
 }
