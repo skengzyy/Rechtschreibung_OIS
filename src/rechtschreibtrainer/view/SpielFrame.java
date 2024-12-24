7c89a0ee6ad1f778b434b3ad8296ecf5a88a587e
skeng <ijerryson@student.tgm.ac.at>
2024-12-24 20:48:30 +0100

    Increased width and height

--- src/rechtschreibtrainer/view/SpielFrame.java
+++ src/rechtschreibtrainer/view/SpielFrame.java
@@ -5,11 +5,10 @@ import java.awt.*;
 
 public class SpielFrame extends JFrame {
     public SpielFrame(JPanel panel) {
-        super("Spielmodus");
         this.add(panel);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(400, 250);
-        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(HIDE_ON_CLOSE);
+        this.setSize(800, 500);
+        this.setLocationRelativeTo((Component)null);
         this.setVisible(false);
     }
 }
